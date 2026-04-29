4c5d5266563ee9226621d3fa6ff4a13049f78acc
= <tejassinghbhati077@gmail.com>
2026-04-29 19:28:36 +0530

    feat: initialize shocks module

---
+++ oeecf/shocks/__init__.py
@@ -0,0 +1,4 @@
+from .labour import calculate_labour_supply_shock
+from .productivity import calculate_productivity_shock
+
+__all__ = ["calculate_labour_supply_shock", "calculate_productivity_shock"]
